563941bfe7f614c929ec1fbf145b3ec8f9baa819
Peter Ullrich <dotup.software@gmail.com>
2019-04-04 13:21:20 +0200

    BusMessage payload

--- src/BusMessage.ts
+++ src/BusMessage.ts
@@ -3,6 +3,7 @@ import { IBusMessage } from './IBusMessage';
 export class BusMessage<T> implements IBusMessage<T> {
   constructor(topic: string) {
     this.topic = topic;
+    this.payload = <T>{};
   }
   topic: string;
   payload: T;
